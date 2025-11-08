6cc75c6dae3dfa1f828fd6dee6a195f8936dfed8
Marsyus <miadolomboy0917@gmail.com>
2025-11-09 02:04:20 +0800

    Created class for Deque, began working on its mechanics

--- static/deques.py
+++ static/deques.py
@@ -1,3 +1,65 @@
 from node import Node
 
-#wala pa - marx
+class Deque:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue_front(self, data):  # Insert at front
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def enqueue_rear(self, data):  # Insert at end
+        new_node = Node(data)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def dequeue_front(self):  # Remove at front
+        if self.head:
+            data = self.head.data
+            current_node = self.head
+            self.head = current_node.next
+            current_node.next = None
+
+            if self.head is None:
+                self.tail = None
+            return data
+        return None
+
+    def dequeue_rear(self):  # Remove at end
+        if self.tail:
+            data = self.tail.data
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                current_node = self.head
+                while current_node.next != self.tail:
+                    current_node = current_node.next
+                current_node.next = None
+                self.tail = current_node
+            return data
+        return None
+
+    def is_empty(self):  # Check if empty
+        return self.head is None
+
+    # Remove at specific parts
+
+    def clear(self):  # Clear all
+        self.head = None
+        self.tail = None
+
+    def length(self):  # Count length of deque
+        count = 0
+        # Dipa tapos
+
