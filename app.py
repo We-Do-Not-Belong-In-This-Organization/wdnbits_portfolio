7c63f54bd7e0d903915e4c98ca953d0de4009b8d
Marsyus <miadolomboy0917@gmail.com>
2025-11-08 13:47:11 +0800

    Added home route

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+# HOME
+@app.route('/')
+def home():
+    return render_template('index.html', header_title="Home Page")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
