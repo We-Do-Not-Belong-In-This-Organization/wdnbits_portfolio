544abd97a696c761310f450ff9518547b7cd1c62
Marsyus <miadolomboy0917@gmail.com>
2025-11-23 00:08:48 +0800

    Fixed once again the routing of back button for each works profile

--- app.py
+++ app.py
@@ -52,19 +52,28 @@ def queue_page():
 @website.route('/enqueue', methods=['POST'])
 def enqueue():
     item = request.form.get('user_enqueue')
+    from_page = request.form.get('from_page')
+
     if item:
         queue_line.enqueue(item)
-    return redirect(url_for('queue_page'))
+
+    return redirect(url_for('queue_page', from_page=from_page))
 
 @website.route('/dequeue', methods=['POST'])
 def dequeue():
+    from_page = request.form.get('from_page')
+
     queue_line.dequeue()
-    return redirect(url_for('queue_page'))
+
+    return redirect(url_for('queue_page', from_page=from_page))
 
 @website.route('/clear_queue', methods=['POST'])
 def clear_queue():
+    from_page = request.form.get('from_page')
+
     queue_line.clear()
-    return redirect(url_for('queue_page'))
+
+    return redirect(url_for('queue_page', from_page=from_page))
 
 
 # =====================================================
@@ -80,37 +89,52 @@ def deque_page():
 @website.route('/enqueue_front', methods=['POST'])
 def enqueue_front():
     item = request.form.get('user_enqueue')
+    from_page = request.form.get('from_page')
+
     if item:
         deque_line.enqueue_front(item)
-    return redirect(url_for('deque_page'))
+
+    return redirect(url_for('deque_page', from_page=from_page))
 
 
 @website.route('/dequeue_rear', methods=['POST'])
 def dequeue_rear():
+    from_page = request.form.get('from_page')
+
     if not deque_line.is_empty():
         deque_line.dequeue_rear()
-    return redirect(url_for('deque_page'))
+
+    return redirect(url_for('deque_page', from_page=from_page))
 
 
 @website.route('/enqueue_rear', methods=['POST'])
 def enqueue_rear():
     item = request.form.get('user_enqueue')
+    from_page = request.form.get('from_page')
+
     if item:
         deque_line.enqueue_rear(item)
-    return redirect(url_for('deque_page'))
+
+    return redirect(url_for('deque_page', from_page=from_page))
 
 
 @website.route('/dequeue_front', methods=['POST'])
 def dequeue_front():
+    from_page = request.form.get('from_page')
+
     if not deque_line.is_empty():
         deque_line.dequeue_front()
-    return redirect(url_for('deque_page'))
+
+    return redirect(url_for('deque_page', from_page=from_page))
 
 
 @website.route('/clear_dob_queue', methods=['POST'])
 def clear_dob_queue():
+    from_page = request.form.get('from_page')
+
     deque_line.clear()
-    return redirect(url_for('deque_page'))
+
+    return redirect(url_for('deque_page', from_page=from_page))
 
 
 
