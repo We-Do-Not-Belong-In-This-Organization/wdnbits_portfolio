98af76e92a220852037f7a106a0876cdf1d3dbb2
mob99-9 <jamesorozco20050@gmail.com>
2025-12-10 14:29:01 +0800

    delete and max value on BST

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -23,7 +23,7 @@ class BinarySearchTree:
         else:
             return self.search_bst(node.right, value)
 
-    def get_max_value(self, node):
+    def max_value(self, node):
         """Return the maximum value in a BST."""
         if node is None:
             return None
@@ -31,3 +31,36 @@ class BinarySearchTree:
         while current.right is not None:
             current = current.right
         return current.data
+    
+    def min_value(self, node): # needed for delete function
+        if node is None:
+            return None
+        current = node 
+        while current.left is not None:
+            current = current.left
+        return current.left
+    
+    def delete(self, node, value):
+        if node is None:
+            return node
+        if node.data > value:
+            self.delete(node.left, value)
+        elif node.data < value:
+            self.delete(node, value)
+        else:
+            minimum_value = self.min_value(node)
+            node.data = minimum_value.data
+            node.right = self.delete(node.right,minimum_value.data)
+
+        return node
+
+
+    def find_height(self, node):
+        if node is None:
+            return 0
+        left = self.find_height(node.left)
+        right = self.find_height(node.right)
+        return max(left, right) + 1  # finds each level until left or right is None and then picks the higher number
+                  
+
+
